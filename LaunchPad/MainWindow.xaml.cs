18b1ac3f2123ff50fd43a66abcde7105081f09fe
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2025-12-22 20:47:45 +0000

    Add project files.

---
+++ LaunchPad/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LaunchPad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class SystemFuncs() 
+        {
+            //A class for callable program functions
+            public void OpenFE()
+            {
+                Process.Start("explorer.exe",
+                    @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs");
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SystemFuncs Funcs = new SystemFuncs();
+            MessageBox.Show("Hey I have Been Clicked!");
+            Funcs.OpenFE();
+
+        }
+    }
+}
