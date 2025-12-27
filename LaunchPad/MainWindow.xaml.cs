7efab879d1e80ebe15e398dea759c2dca26eb6e2
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2025-12-27 18:31:22 +0000

    Adding launcher functionality. Apps can be added to the program and launched

--- LaunchPad/MainWindow.xaml.cs
+++ LaunchPad/MainWindow.xaml.cs
@@ -20,39 +20,88 @@ namespace LaunchPad
     /// </summary>
     public partial class MainWindow : Window
     {
-        public class SystemFuncs() 
+        public class SystemFuncs 
         {
             //A class for callable program functions
-            public void OpenFE()
+            public void OpenFE() //Funciton opens file explorer for user to click and add apps to Launcher
             {
                 //Specifying directory to load into when called
                 const string DefDir = @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\";
                 OpenFileDialog fileDialog = new OpenFileDialog();
                 fileDialog.InitialDirectory = DefDir;
-                //Filter
+                //Filter to EXEs and Shortcuts
                 fileDialog.Filter = "Shortcut (*.lnk) |*.lnk|Executable (*.exe) | *.exe";
                 fileDialog.Title = "Add An App To LaunchPad";
                 fileDialog.ShowDialog();
 
+                //Create string of users input
                 string FilePath = fileDialog.FileName;
                 //string fileExtension = System.IO.Path.GetExtension(FilePath);
 
-                // Example usage
-                MessageBox.Show($"Extension: {FilePath}");
+                //Condition
+                if (FilePath == "") //Null input - notify user
+                {
+                    MessageBox.Show($"User did not select a path!");
+                    return;
+                }
+                else //Else - Continue with function logic
+                {
+                    //Store file paths in a text file
+                    File.AppendAllText("ProgsLaunch.txt", FilePath + Environment.NewLine);
 
+                }
+            }
+
+            public void Launcher() //Function to launch selected apps -  this will read the text file and launch the stored paths
+            {
+                //Read the text file for stored paths
+                string TextPath = "ProgsLaunch.txt";
+
+                if(!File.Exists(TextPath))
+                {
+                    MessageBox.Show("LaunchPad has no apps to launch! Please add apps to the launcher");
+                    return;
+                }
+
+                string[] StoredPaths = File.ReadAllLines(TextPath);
+
+                foreach (string Path in StoredPaths)
+                {
+                    try
+                    { 
+                        var ProcessStarter = new ProcessStartInfo
+                        {
+                            FileName = Path,
+                            UseShellExecute = true
+                        };
+
+                        Process.Start(ProcessStarter);
+                    }
+                    catch (System.Exception ex)
+                    {
+                        MessageBox.Show($"Error launching {Path} : {ex.Message}");
+                    }
+                }
 
             }
+
         }
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public void Button_Click(object sender, RoutedEventArgs e)
+        public void Button_Click_Add(object sender, RoutedEventArgs e)
         {
             SystemFuncs Funcs = new SystemFuncs();
             Funcs.OpenFE();
 
+        }
+        public void Button_Click_Launch(object sender, RoutedEventArgs e)
+        {
+            SystemFuncs Funcs = new SystemFuncs();
+            MessageBox.Show($"Launch!");
+            Funcs.Launcher();
         }
     }
 }
