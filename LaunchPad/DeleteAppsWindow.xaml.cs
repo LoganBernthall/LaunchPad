34708a74745b7381d3657f24416fd0d94d795c49
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2026-01-04 18:08:19 +0000

    * Added icon to delete app windows form. * Removed WindowState="Minimized" from delete apps windows form. Resulted in form being hidden behind main. * Added listview to show programs that can be removed from LaunchPad.

--- LaunchPad/DeleteAppsWindow.xaml.cs
+++ LaunchPad/DeleteAppsWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace LaunchPad
 {
@@ -17,9 +18,41 @@ namespace LaunchPad
     /// </summary>
     public partial class DeleteAppsWindow : Window
     {
+
+        private void LoadListView()
+        {
+            //Code to load ListView with apps from ProgsLaunch.txt
+            //Function also cuts the path down slightly
+            ListViewAppsToDel.Items.Clear();
+
+            foreach (string FullProgPath in File.ReadAllLines("ProgsLaunch.txt"))
+            {
+                if (string.IsNullOrWhiteSpace(FullProgPath)) continue;
+
+                string displayName = System.IO.Path.GetFileName(FullProgPath);
+
+                ListViewAppsToDel.Items.Add(displayName);
+            }
+
+        }
+
+
         public DeleteAppsWindow()
         {
             InitializeComponent();
+            LoadListView();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+
+        }
+
+        private void BtnDeleteFromLP_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
